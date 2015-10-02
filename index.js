24c698018529562223560781753d84c83dc93ac4
Rasmus Eskola <fruitiex@gmail.com>
2015-10-03 00:58:31 +0300

    TODO: gamestate

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
+var _ = require('underscore');
+var io = require('socket.io')();
+var livereload = require('livereload');
+var server = livereload.createServer();
+server.watch(__dirname + '/public');
+
+var width = 10;
+var height = 10;
+
+var gameState = [];
+
+for (var i = 0; i < height; i++) {
+    gameState.push([]);
+}
+
+var numPlayers = 4;
+
+for (var x = 0; x < width; x++) {
+    for (var y = 0; y < height; y++) {
+        gameState[x][y] = Math.floor(Math.random() * numPlayers);
+    }
+}
+
+console.log(gameState);
+
+var doMove = function(from, direction) {
+    var temp = gameState[from.x][from.y];
+
+    switch (direction) {
+        case 'up':
+            gameState[from.x][from.y] = gameState[from.x][from.y - 1];
+            gameState[from.x][from.y - 1] = temp;
+            break;
+        case 'down':
+            gameState[from.x][from.y] = gameState[from.x][from.y + 1];
+            gameState[from.x][from.y + 1] = temp;
+            break;
+        case 'left':
+            gameState[from.x][from.y] = gameState[from.x - 1][from.y];
+            gameState[from.x - 1][from.y] = temp;
+            break;
+        case 'right':
+            gameState[from.x][from.y] = gameState[from.x + 1][from.y];
+            gameState[from.x + 1][from.y] = temp;
+            break;
+        default:
+            console.log('invalid move received! ' + direction);
+    }
+};
+
 var token = require(process.env.HOME + '/.diagram-bot-token.js');
 var Bot = require('node-telegram-bot');
 
