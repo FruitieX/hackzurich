f01cdc3079c34cf5482d443a7558f959e757a6aa
Rasmus Eskola <fruitiex@gmail.com>
2015-10-03 01:58:08 +0300

    WIP: moving playfield

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 var _ = require('underscore');
 var io = require('socket.io')();
 
-io.on('connection', function(socket) {
-    socket.emit('testevent', 'hello world');
-});
-
 var livereload = require('livereload');
 var server = livereload.createServer();
 server.watch(__dirname + '/public');
@@ -17,7 +13,7 @@ app.use(express.static(__dirname + '/public'));
 app.listen(8080);
 
 var width = 10;
-var height = 10;
+var height = 5;
 
 var gameState = [];
 
@@ -29,36 +25,87 @@ var numPlayers = 4;
 
 for (var x = 0; x < width; x++) {
     for (var y = 0; y < height; y++) {
-        gameState[x][y] = Math.floor(Math.random() * numPlayers);
+        gameState[y][x] = Math.floor(Math.random() * numPlayers);
     }
 }
 
-console.log(gameState);
+var drawGameState = function() {
+    console.log(gameState);
+    /*
+    for (var y = 0; y < height; y++) {
+        for (var x = 0; x < width; x++) {
+            process.stdout.write(gameState[x][y] + ', ');
+        }
+        process.stdout.write('\n');
+    }
+    */
+};
+drawGameState();
 
 var doMove = function(from, direction) {
-    var temp = gameState[from.x][from.y];
+    var temp = gameState[from.y][from.x];
 
     switch (direction) {
-        case 'up':
-            gameState[from.x][from.y] = gameState[from.x][from.y - 1];
-            gameState[from.x][from.y - 1] = temp;
+        case 'u':
+            if (from.y <= 0) {
+                console.log('invalid move!');
+                return;
+            }
+
+            gameState[from.y][from.x] = gameState[from.y - 1][from.x];
+            gameState[from.y - 1][from.x] = temp;
+
+            var to = {x: from.x, y: from.y - 1};
+            io.sockets.emit('doMove', [from, to]);
+            drawGameState();
             break;
-        case 'down':
-            gameState[from.x][from.y] = gameState[from.x][from.y + 1];
-            gameState[from.x][from.y + 1] = temp;
+        case 'd':
+            if (from.y > height - 1) {
+                console.log('invalid move!');
+                return;
+            }
+
+            gameState[from.y][from.x] = gameState[from.y + 1][from.x];
+            gameState[from.y + 1][from.x] = temp;
+
+            var to = {x: from.x, y: from.y + 1};
+            io.sockets.emit('doMove', [from, to]);
+            drawGameState();
             break;
-        case 'left':
-            gameState[from.x][from.y] = gameState[from.x - 1][from.y];
-            gameState[from.x - 1][from.y] = temp;
+        case 'l':
+            if (from.x <= 0) {
+                console.log('invalid move!');
+                return;
+            }
+
+            gameState[from.y][from.x] = gameState[from.y][from.x - 1];
+            gameState[from.y][from.x - 1] = temp;
+
+            var to = {x: from.x - 1, y: from.y};
+            io.sockets.emit('doMove', [from, to]);
+            drawGameState();
             break;
-        case 'right':
-            gameState[from.x][from.y] = gameState[from.x + 1][from.y];
-            gameState[from.x + 1][from.y] = temp;
+        case 'r':
+            if (from.x > width - 1) {
+                console.log('invalid move!');
+                return;
+            }
+
+            gameState[from.y][from.x] = gameState[from.y][from.x + 1];
+            gameState[from.y][from.x + 1] = temp;
+
+            var to = {x: from.x + 1, y: from.y};
+            io.sockets.emit('doMove', [from, to]);
+            drawGameState();
             break;
         default:
             console.log('invalid move received! ' + direction);
     }
 };
+
+io.on('connection', function(socket) {
+    socket.emit('gameState', gameState);
+});
 
 var token = require(process.env.HOME + '/.diagram-bot-token.js');
 var Bot = require('node-telegram-bot');
@@ -69,6 +116,11 @@ var bot = new Bot({
 .on('message', function(msg) {
     if (msg.text) {
         console.log('got TG message: ' + msg.text);
+        var x = msg.text.charCodeAt(0) - 'a'.charCodeAt(0);
+        var y = msg.text.charCodeAt(1) - '0'.charCodeAt(0);
+        var dir = msg.text.substr(2, 3);
+
+        doMove({x: x, y: y}, dir);
     }
 });
 
